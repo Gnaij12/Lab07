5418e0485588646378ec052b6be1f301d56744a6
1532269 <alanzjiang10@gmail.com>
2022-10-13 11:56:08 -0400

    kirby2

--- app/src/main/java/com/example/lab07/KirbyFragment.java
+++ app/src/main/java/com/example/lab07/KirbyFragment.java
@@ -1,12 +1,23 @@
 package com.example.lab07;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class KirbyFragment extends androidx.fragment.app.Fragment {
     int[] images = new int[]{R.drawable.carby, R.drawable.curby,
             R.drawable.beeg_kirby, R.drawable.kirby_road,R.drawable.kirby_background,R.drawable.kirby_costume,R.drawable.kirby_real_face};
+    ImageView kirby;
     int iKirby;
     public static KirbyFragment newInstance(int iKirby) {
         KirbyFragment kirbyFrag = new KirbyFragment();
@@ -19,6 +30,17 @@ public class KirbyFragment extends androidx.fragment.app.Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         iKirby = getArguments().getInt("iKirby",0);
+    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.kirby_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        kirby = view.findViewById(R.id.background);
+        kirby.setScaleType(ImageView.ScaleType.FIT_XY);
+        kirby.setImageResource(images[iKirby]);
 
     }
 }
